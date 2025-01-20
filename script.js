fddae74a607456f1456a56060269e4c83ee62e33
bhpuja <vpuja0421@gmail.com>
2025-01-20 16:17:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function compareNumbers() {
+    // Prompt the user to input two numbers
+    let number1 = prompt("Enter the first number:");
+    let number2 = prompt("Enter the second number:");
+
+    // Convert the inputs to numbers
+    number1 = Number(number1);
+    number2 = Number(number2);
+
+    // Compare the numbers and display the result in an alert box
+    let isEqual = (number1 === number2);
+    alert(isEqual);
+}
